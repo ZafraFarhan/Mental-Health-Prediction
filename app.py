12ae57f497f6531545e3b99ab28459d18f3e570e
Zafra Farhan <145136283+ZafraFarhan@users.noreply.github.com>
2024-11-01 09:51:39 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,135 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Set up the page config
+st.set_page_config(page_title="Mental Health Dashboard", layout="wide")
+
+st.markdown("""
+    <head>
+        <meta charset="UTF-8">
+        <title>All Navigation Menu Hover Animation | CodingLab</title> 
+        <style>
+            [data-testid=stSidebar] {
+                background-color: #000000;
+            }
+            body {
+                background-color: #2E2E2E;  /* Black background color */
+                
+            }
+            .stApp {
+                background-color: #2E2E2E;  /* Set background to black for the app */
+                
+            }
+            h1, h2, h3, h4, h5, h6, p, li, a {
+                color: #ffffff;  /* Set font color to white for all text elements */
+            }
+
+
+            .stButton button {
+                background-color: #FF7F27;
+                color: #ffffff !important;
+                border-radius: 10px;
+                padding: 10px 20px;
+                font-size: 16px;
+            }
+                
+        </style>
+    </head>
+    """, unsafe_allow_html=True)
+
+# Load data
+def load_data():
+    data = pd.read_csv("df_new.csv")
+    return data
+df = load_data()
+
+
+
+st.title("Mental Health Dashboard")
+
+# Instruction text
+st.markdown("""
+    Details based on the measures below:
+    - **1:** Never
+    - **2:** Always
+    - **3:** Often
+    - **4:** Rarely
+    - **5:** Sometimes
+    - **6:** Not at all
+    """)
+
+with st.sidebar:
+    st.header("⚙️ Settings")
+    
+    feature = st.radio(
+        "Select Feature",
+        ('Sadness', 'Euphoric', 'Exhausted', 'Sleep dissorder', 'Mood Swing',
+       'Suicidal thoughts', 'Anorxia', 'Authority Respect', 'Try-Explanation',
+       'Aggressive Response', 'Ignore & Move-On', 'Nervous Break-down',
+       'Admit Mistakes', 'Overthinking', 'Sexual Activity', 'Concentration',
+       'Optimisim'),
+    )
+
+
+st.subheader(f"Distribution of {feature}")
+
+# Generate chart based on selection
+plt.figure(figsize=(10, 6), facecolor='#2E2E2E')  # Set the figure background color
+
+# Calculate percentage of each category
+counts = df[feature].value_counts()
+percentages = counts / counts.sum() * 100
+
+# Create the bar plot
+bar_plot = sns.barplot(x=percentages.index, 
+                       y=percentages.values, 
+                       color='#FFA001')  # Set bar color
+
+plt.xlabel(feature, color='white')  # Set x-label color
+plt.ylabel("Percentage (%)", color='white')   # Set y-label color
+plt.gca().set_facecolor('#2E2E2E') 
+plt.xticks(color='white')  # Set x-tick color
+plt.yticks(color='white')  # Set y-tick color
+plt.ylim(0, 100)  # Set y-axis limits from 0 to 100
+
+# Add percentage labels on top of the bars
+for p in bar_plot.patches:
+    height = p.get_height()
+    plt.text(p.get_x() + p.get_width() / 2, 
+             height, 
+             f'{height:.1f}%', 
+             ha='center', 
+             color='white', 
+             fontsize=12, 
+             va='bottom')  # va='bottom' to place text above the bar
+
+st.pyplot(plt)
+
+# Create a DataFrame to hold the percentage of each feature for each depression state
+df_grouped = df.groupby('Expert Diagnose')[feature].value_counts(normalize=True).unstack(fill_value=0) * 100
+
+# Generate charts for each depression state
+plt.figure(figsize=(10, 6), facecolor='#2E2E2E')
+df_grouped.plot(kind='bar', stacked=True, color=sns.color_palette("husl", len(df_grouped.columns)), ax=plt.gca())
+
+st.subheader(f'Distribution of {feature} by Expert Diagnose')
+
+plt.xlabel('Expert Diagnose', color='white')
+plt.ylabel('Percentage (%)', color='white')
+plt.gca().set_facecolor('#2E2E2E')
+plt.xticks(color='white')  
+plt.yticks(color='white')  
+plt.ylim(0, 100)  
+
+# Add percentage labels on top of the bars
+for container in plt.gca().containers:
+    plt.bar_label(container, fmt='%.1f%%', color='white', fontsize=12)
+
+
+# Customizing x-axis tick labels
+depression_labels = ['Bipolar Type-1', 'Bipolar Type-2', 'Depression', 'Normal']
+plt.xticks(ticks=range(len(depression_labels)), labels=depression_labels, rotation=45, ha='right', color='white')
+
+st.pyplot(plt)
---
+++ app1.py
@@ -0,0 +1,178 @@
+import streamlit as st
+import pandas as pd
+from PIL import Image
+from datetime import datetime  # Import datetime
+import pickle
+
+
+
+with open('rf_model.pkl', 'rb') as file:
+    pipeline = pickle.load(file)
+
+def run():
+    st.set_page_config(layout="wide")
+
+    
+   
+    st.markdown("""
+    <head>
+        <meta charset="UTF-8">
+        <title>All Navigation Menu Hover Animation | CodingLab</title> 
+        <style>
+            body {
+                background-color: #000000;  /* Black background color */
+                
+            }
+            .stApp {
+                background-color: #000000;  /* Set background to black for the app */
+                
+            }
+            h1, h2, h3, h4, h5, h6, p, li, a, label {
+                color: #ffffff;  /* Set font color to white for all text elements */
+
+            }
+  
+            .center::after, .upward::after, .forward::after {
+                content: '';
+                position: absolute;
+                width: 100%;
+                height: 2px;
+                bottom: -5px;
+                left: 0;
+                background-color: #fff;  /* Change underline color to white */
+                transform: scaleX(0);
+                transition: transform 0.3s ease;
+            }
+            .center::after {
+                transform-origin: center;
+            }
+            .upward::after {
+                transform-origin: bottom;
+            }
+            .forward::after {
+                transform-origin: top right;
+            }
+            .nav-links li:hover::after {
+                transform: scaleX(1);
+            }
+            
+            .stSlider > div > div > div > div {
+                background-color: #FF7F27;
+            }
+            .stSlider > div > div > div > input {
+                background-color: #FF7F27;
+                border: none;
+            }
+            .stButton button {
+                background-color: #FF7F27;
+                color: #ffffff !important;
+                border-radius: 10px;
+                padding: 10px 20px;
+                font-size: 16px;
+            }
+                
+        </style>
+    </head>
+    """, unsafe_allow_html=True)
+
+    st.markdown(
+    """<style>
+    div[class*="stRadio"] > label > div[data-testid="stMarkdownContainer"] > p {
+        font-size: 18px;
+        margin-bottom: 15px;
+    }
+    div[class*="stSlider"] > label > div[data-testid="stMarkdownContainer"] > p {
+        font-size: 18px;
+        margin-bottom: 15px;
+    }
+    </style>
+    """, unsafe_allow_html=True
+    )
+
+    
+    st.header("Mental Health Prediction")
+
+    mapping = {
+    'Seldom': 1,
+    'Sometimes': 2,
+    'Most-Often': 4,
+    'Usually': 3
+    }
+
+
+        # Inputs for user data
+    MoodSwings = st.radio("Mood Swings: Occurrences of sudden and unpredictable mood changes", ("YES", "NO"))
+    Optimisim = st.slider("Optimisim: Level of positive outlook towards future outcomes", 1, 5, 1)
+    SuicidalThoughts = st.radio("Suicidal Thoughts: Frequency of having thoughts about self-harm or suicide", ("YES", "NO"))
+    Sadness = st.radio("Sadness: Frequency of experiencing feelings of unhappiness or despair", ('Seldom', 'Sometimes','Most-Often','Usually'))
+    Sadness_value = mapping[Sadness]
+    Euphoric = st.radio("Euphoria: Frequency of experiencing intense feelings of happiness or excitement", ('Seldom', 'Sometimes','Most-Often','Usually'))
+    Euphoric_value = mapping[Euphoric]
+    AggressiveResponse = st.radio("Aggressive Response: Tendency to react with anger or hostility", ("YES", "NO"))
+    SleepDisorder = st.radio("Sleep Disorder: Issues or disruptions in regular sleep patterns", ('Seldom', 'Sometimes','Most-Often','Usually'))
+    SleepDisorder_value = mapping[SleepDisorder]
+    Exhausted = st.radio("Exhaustion: Level of physical or mental tiredness experienced.", ('Seldom', 'Sometimes','Most-Often','Usually'))
+    Exhausted_value = mapping[Exhausted]
+    AuthorityRespect = st.radio("Authority Respect: Level of regard or compliance shown towards figures of authority", ("YES", "NO"))
+    Concentration = st.slider("Concentration: Ability to focus and stay attentive on tasks", 1, 5, 1)
+    SexualActivity = st.slider("Sexual Activity: Frequency of engaging in sexual behavior", 1, 5, 1)
+    NervousBreakdown = st.radio("Nervous Breakdown: Experience of mental or emotional collapse due to stress", ("YES", "NO"))
+    Anorxia = st.radio("Anorexia: Tendency to avoid or lose interest in eating", ("YES", "NO"))
+    Overthinking = st.radio("Overthinking: Frequency of excessive or repetitive thoughts", ("YES", "NO"))
+    TryExplanation = st.radio("Try-Explaining: Willingness to explain oneself in challenging situations", ("YES", "NO"))
+    AdmitMistakes = st.radio("Admitting Mistakes: Openness to acknowledge personal errors or faults", ("YES", "NO"))
+    IgnoreNMoveOn = st.radio("Ignore & Move-On: Tendency to overlook issues and move forward", ("YES", "NO"))
+
+
+
+    def make_prediction(input_data):
+        # Convert input data to DataFrame
+        input_df = pd.DataFrame([input_data])
+        
+        # Print the input data for debugging
+        print("Input Data:\n", input_df)
+        
+        # Make prediction using the loaded pipeline
+        prediction = pipeline.predict(input_df)
+        
+        # Print the prediction for debugging
+        print("Prediction:\n", prediction)
+        
+        return prediction
+
+
+    input_data = {
+        "Mood Swing": MoodSwings,
+        "Optimisim": Optimisim,
+        "Suicidal thoughts": SuicidalThoughts,
+        "Sadness": Sadness_value,
+        "Euphoric": Euphoric_value,
+        "Aggressive Response": AggressiveResponse,
+        "Sleep dissorder": SleepDisorder_value,
+        "Exhausted": Exhausted_value,
+        "Authority Respect": AuthorityRespect,
+        "Concentration": Concentration,
+        "Sexual Activity": SexualActivity,
+        "Nervous Break-down": NervousBreakdown,
+        "Anorxia": Anorxia,
+        "Overthinking": Overthinking,
+        "Try-Explanation": TryExplanation,
+        "Admit Mistakes": AdmitMistakes,
+        "Ignore & Move-On": IgnoreNMoveOn,
+
+    }
+
+    if st.button('Predict'):
+        prediction = make_prediction(input_data)
+        print("Prediction:", prediction)
+        
+        if prediction[0] == 'Normal':
+            st.write("Normal")
+        elif prediction[0] == 'Bipolar Type-2':
+            st.write("Bipolar Type-2")
+        elif prediction[0] == 'Bipolar Type-1':
+            st.write("Bipolar Type-1")
+        else:
+            st.write("Depression")
+            
+run()
